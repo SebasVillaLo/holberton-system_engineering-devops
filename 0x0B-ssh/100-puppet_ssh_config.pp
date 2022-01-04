eaf6105bbcc5864f7279bba85b1805e1925e1c30
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-04 17:08:57 -0500

    add proyect

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # config ssh
 include stdlib
-
 file_line {'identity file':
     path    => '/etc/ssh/ssh_config',
-    line    => 'IdentityFile ~/.ssh/school',
+    line    => '    IdentityFile ~/.ssh/school',
     replace => true,
 }
+
 file_line {'password no':
     path    => '/etc/ssh/ssh_config',
-    line    => 'PasswordAuthentication no',
+    line    => '    PasswordAuthentication no',
     replace => true,
 }
