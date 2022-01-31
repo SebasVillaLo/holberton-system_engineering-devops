0e76a6fe65079b474ff75c563516b4cdff30c4e3
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-31 17:39:46 -0500

    add proyect

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,14 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
-    user: int = argv[1]
+    user = int(argv[1])
     peticion = requests.get('{}users/{}'.format(url, user)).json()
     name = peticion.get('name')
     taskall = requests.get('{}todos?userId={}'.format(url, user)).json()
