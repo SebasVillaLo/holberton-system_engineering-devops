b8bf777b477cb6c147e3ba8ecb66c436f8e257b2
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-31 15:41:22 -0500

    add proyect

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+from sys import argv
+
+url = 'https://jsonplaceholder.typicode.com/'
+user: int = argv[1]
+peticion = requests.get('{}users/{}'.format(url, user)).json()
+name = peticion.get('name')
+taskall = requests.get('{}todos?userId={}'.format(url, user)).json()
+
+
+taskList = []
+for task in taskall:
+    if task.get('completed') is True:
+        taskList.append(task.get('title'))
+
+
+print("Employee {} is done with tasks({}/{})"
+      .format(name, len(taskList), len(taskall)))
+print("\n".join('\t {}'.format(task) for task in taskList))
