a3de310e7557841bf77ff18472eb8c634eaa1013
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-31 15:46:17 -0500

    add proyect

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,21 +2,23 @@
 """
 This script is for get the name and finished task of the empleate
 """
+
 import requests
 from sys import argv
 
-url = 'https://jsonplaceholder.typicode.com/'
-user: int = argv[1]
-peticion = requests.get('{}users/{}'.format(url, user)).json()
-name = peticion.get('name')
-taskall = requests.get('{}todos?userId={}'.format(url, user)).json()
+if __name__ == "__main__":
 
+    url = 'https://jsonplaceholder.typicode.com/'
+    user: int = argv[1]
+    peticion = requests.get('{}users/{}'.format(url, user)).json()
+    name = peticion.get('name')
+    taskall = requests.get('{}todos?userId={}'.format(url, user)).json()
 
-taskList = []
-for task in taskall:
-    if task.get('completed') is True:
-        taskList.append(task.get('title'))
+    taskList = []
+    for task in taskall:
+        if task.get('completed') is True:
+            taskList.append(task.get('title'))
 
-print("Employee {} is done with tasks({}/{})"
-      .format(name, len(taskList), len(taskall)))
-print("\n".join('\t {}'.format(task) for task in taskList))
+    print("Employee {} is done with tasks({}/{})"
+          .format(name, len(taskList), len(taskall)))
+    print("\n".join('\t {}'.format(task) for task in taskList))
