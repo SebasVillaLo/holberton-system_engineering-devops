31bfb7a81e21c9160618ed37fa2e117e8011d501
SebasVillaLo <villalopezsebastian@gmail.com>
2022-01-31 22:40:54 -0500

    add proyect

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,17 +8,17 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    peticion = requests.get('{}users'.format(url)).json()
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("{}users".format(url)).json()
 
-    with open('todo_all_employees.json', 'w') as f:
+    with open("todo_all_employees.json", "w") as f:
         json.dump({
             u.get("id"): [{
-                "task": t.get('title'),
-                "completed": t.get('completed'),
-                "username": u.get('name')
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
             } for t in requests.get(url + "todos",
                                     params={
                                         "userId": u.get("id")
                                     }).json()]
-            for u in peticion}, f)
+            for u in users}, f)
