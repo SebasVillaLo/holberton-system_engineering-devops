63001a89f0ca772e40c2e0435106fed760c653c0
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-16 16:57:31 -0500

    ADD: Proyect 0x18

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Show in console the ten hot titles
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Return 0 if failed
+    """
+    count = 1
+    headers = {'User-Agent': 'MyHolbertonAPI/0.0.1'}
+    response = requests.get('https://www.reddit.com/r/{}/hot.json'.
+                            format(subreddit), headers=headers)
+    if (response.status_code == 200 and
+            response.json()['data']['children'] != []):
+        for value in response.json()['data']['children']:
+            if (count <= 10):
+                print(value['data']['title'])
+                count += 1
+            else:
+                break
+    else:
+        print(None)
