98fa0a8eb0ec94146acdb86d321e83a12a212591
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-22 19:36:06 -0500

    ADD: task 0 and readme

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+
+exec {'fix-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path => '/usr/local/bin/:/bin/'
+}
+
+exec {'nginx-restart':
+  command => 'nginx restart',
+  path => '/usr/local/bin/:/bin/'
+}
