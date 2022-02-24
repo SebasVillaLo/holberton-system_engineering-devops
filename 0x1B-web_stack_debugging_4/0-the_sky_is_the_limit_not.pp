e73e4cdc4d8b41c4276feb4b9a7ebbb3023a5971
SebasVillaLo <villalopezsebastian@gmail.com>
2022-02-24 12:42:59 -0500

    FIX: comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
-
-exec {'fix-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+# Request limit
+exec {'sed -i "s/15/4096/" /etc/default/nginx':
+  path      => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell
 }
 
-exec {'nginx-restart':
-  command => 'nginx restart',
-  path    => '/usr/local/bin/:/bin/'
+exec {'sudo service nginx restart':
+  path      => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell
 }
